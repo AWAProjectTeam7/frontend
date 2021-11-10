206e67928f7977883c26ba7c79669e188b3b87a7
mhholle <mikko.holappa@luukku.com>
2021-11-10 11:06:33 +0200

    navbar and basic components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import ShoppingCart from './components/ShoppingCart';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -10,10 +13,16 @@ function App() {
       <div>
         <div className="navbar">
           <Link to="/"><div>Home</div></Link>
+          <Link to="login"><div>Login</div></Link>
+          <Link to="register"><div>Register</div></Link>
+          <Link to="cart"><div>Cart</div></Link>
           
         </div>
         <Routes>
           <Route path="/" element={ <Home /> } />
+          <Route path="/login" element = { <Login /> } />
+          <Route path="/register" element = { <Register /> } />
+          <Route path="/cart" element = { <ShoppingCart /> } />
         </Routes>
       </div>
     </BrowserRouter>
