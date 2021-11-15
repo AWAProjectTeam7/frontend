c1f11e668b89fd86b75448f581e5be3f605c71bf
mhholle <77332803+mhholle@users.noreply.github.com>
2021-11-15 08:30:51 +0200

    app name added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import ShoppingCart from './components/ShoppingCart';
+import Logo from './components/Logo';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
@@ -15,6 +16,7 @@ function App() {
       <div>
         <div className="navbar">
           {/* Change visual order with links */}
+          <Logo />
           <Link to="/"><div>Home</div></Link>
           <Link to="login"><div>Login</div></Link>
           <Link to="register"><div>Register</div></Link>
